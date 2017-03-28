43887b0fe9d77befa1385954c32b9e423e44c26d
David Thomas <davidthomas@MacBook-Pro-de-David.local>
2017-03-28 18:45:46 +0200

    Improve the CVector/CMatrix classes and add a test for CVector.

--- include/MatVec.h
+++ include/MatVec.h
@@ -16,26 +16,20 @@ public:
     CVector(void);
     CVector(const unsigned long & size, const double & val = 0.0);
     CVector(const CVector & u);
-    explicit CVector(const unsigned long & size, const double* u_array);
-    virtual ~CVector();
+    ~CVector();
     unsigned long GetSize() const;
     double* GetVec() const;
     void print() const;
-    void Initialize(const double & val);
+    void Initialize(const unsigned long & size, const double & val = 0.0);
+    void SetAllValues(const double & val);
     CVector & operator=(const CVector & u);
-    CVector & operator+(const CVector & u) const;
     CVector & operator+=(const CVector & u);
-    CVector & operator-(const CVector & u) const;
     CVector & operator-=(const CVector & u);
-    CVector & operator*(const double & val) const;
-    //CVector & operator*(const double & val, const CVector & u);
     CVector & operator*=(const double & val);
-    CVector & operator/(const double & val) const;
     CVector & operator/=(const double & val);
     double & operator[](const unsigned long & i) const;
-    //const double & operator[](const unsigned long & i) const;
     double norm() const;
-    double dotProd(const CVector & u, const CVector & v) const;
+    double dotProd(const CVector & v) const;
     void Reset();
 };
 
@@ -44,33 +38,69 @@ class CMatrix {
 protected:
     unsigned long nEq;
     unsigned long nVar;
-    CVector* mat_val;
+    CVector mat_val;
 
 public:
     CMatrix(void);
     CMatrix(const unsigned long & val_nEq, const unsigned long & val_nVar, const double & val = 0.0);
-    explicit CMatrix(const unsigned long & val_nEq, const unsigned long & val_nVar, const double* a_array);
-    virtual ~CMatrix();
+    CMatrix(const CMatrix & A);
+    ~CMatrix();
+    void Initialize(const unsigned long & val_nEq, const unsigned long & val_nVar, const double & val = 0.0);
     void print() const;
-    //void printt() const;
     CMatrix & operator=(const CMatrix & a);
-    CMatrix & operator+(const CMatrix & a) const;
     CMatrix & operator+=(const CMatrix & a);
+    CMatrix & operator-=(const CMatrix & a);
+    CMatrix & operator*=(const double & val);
+    CMatrix & operator/=(const double & val);
     unsigned long GetnEq() const;
     unsigned long GetnVar() const;
     double* GetMat() const;
-    CVector* GetCVec() const;
-    void SetElm(int i, int j, double val);
-    double GetElm(int i, int j) const;
+    CVector GetCVec() const;
+    void SetElm(const int & i, const int & j, double val);
+    double GetElm(const int & i, const int & j) const;
     double DiagProduct() const;
     double ComputeDet() const;
     void Reset();
 };
 
-CVector MatVecProd(CMatrix* A, CVector* b);
-CVector ScalVecProd(const double & scal, CVector* b);
-CMatrix ScalMatProd(const double & scal, CMatrix* A);
-int SolveSys(CMatrix *A, CVector *b);
+CVector MatVecProd(const CMatrix & A, const CVector & b);
+CVector ScalVecProd(const double & scal, const CVector & b);
+CMatrix ScalMatProd(const double & scal, const CMatrix & A);
+int SolveSys(const CMatrix & A, CVector & b);
 
 void MatrixToVec(int order, double** matrix, double* vecteur, int Nrow, int Ncol, int sizeVec);
 void VecToMatrix(int order, double** matrix, double* vecteur, int Nrow, int Ncol, int sizeVec);
+
+/*OPERATOR +*/
+CVector operator+(CVector & vecA, CVector & vecB);
+CVector operator+(const CVector & vecA, const CVector & vecB);
+CVector operator+(CVector & vecA, const CVector & vecB);
+CVector operator+(const CVector & vecA, CVector & vecB);
+CMatrix operator+(const CMatrix & matA, const CMatrix & matB);
+
+
+/*OPERATOR -*/
+CVector operator-(CVector & vecA, CVector & vecB);
+CVector operator-(const CVector & vecA, CVector & vecB);
+CVector operator-(CVector & vecA, const CVector & vecB);
+CVector operator-(const CVector & vecA, const CVector & vecB);
+CMatrix operator-(const CMatrix & matA, const CMatrix & matB);
+
+/*OPERATOR * */
+CVector operator*(CVector & vecA, double & val_mult);
+CVector operator*(CVector & vecA, const double & val_mult);
+CVector operator*(const CVector & vecA, double & val_mult);
+CVector operator*(const CVector & vecA, const double & val_mult);
+CVector operator*(double & val_mult, CVector & vecA);
+CVector operator*(const double & val_mult, CVector & vecA);
+CVector operator*(double & val_mult, const CVector & vecA);
+CVector operator*(const double & val_mult, const CVector & vecA);
+CMatrix operator*(CMatrix matA, double & val);
+CMatrix operator*(double & val, CMatrix matA);
+
+
+/*OPERATOR / */
+CVector operator/(CVector & vecA, double & val_mult);
+CVector operator/(CVector & vecA, const double & val_mult);
+CVector operator/(const CVector & vecA, double & val_mult);
+CVector operator/(const CVector & vecA, const double & val_mult);
--- src/MatVec.cpp
+++ src/MatVec.cpp
@@ -6,7 +6,7 @@ using namespace std;
 
 /*--- Definition of the CVector class ---*/
 
-CVector::CVector(void){
+CVector::CVector(void):nElm(0){
 
   vec_val = NULL;
 
@@ -14,60 +14,58 @@ CVector::CVector(void){
 
 CVector::CVector(const unsigned long & size, const double & val){
 
-  nElm = size;
-
-  if(nElm <= 0){
+  vec_val = NULL;
+  if(size <= 0){
+    nElm = 0;
     cerr << "CVector:CVector(const unsigned long &, const double): " << "invalid input : size = " << size << endl;
     throw(-1);
   }
-
-  vec_val = new double[nElm];
-  for(unsigned int i=0; i < nElm; i++)
-    vec_val[i] = val;
-
+  else{
+    nElm = size;
+    vec_val = new double[nElm];
+    for(unsigned int i=0; i < nElm; i++)
+      vec_val[i] = val;
+  }
 }
 
 CVector::CVector(const CVector & u){
 
   nElm = u.nElm;
-
+  vec_val = NULL;
   vec_val = new double[nElm];
   for(unsigned int i = 0; i < nElm; i++)
     vec_val[i] = u.vec_val[i];
 }
 
-CVector::CVector(const unsigned long & size, const double* u_array){
-
-  nElm = size;
-
-  if(nElm <= 0){
-    cerr << "CVector::CVector(const unsigned long &, const double): " << "invalid input: size = " << size << endl;
-    throw(-1);
-  }
-
-  vec_val = new double[nElm];
-  for(unsigned int i=0; i<nElm; i++)
-    vec_val[i] = u_array[i];
-}
-
 CVector::~CVector(){
-  delete [] vec_val;
-  vec_val = NULL;
-  nElm = -1;
+
+  if (vec_val != NULL) delete [] vec_val;
 }
 
+void CVector::Initialize(const unsigned long &size, const double & val){
 
-void CVector::Initialize(const double & val){
-  if(nElm <= 0){
-    cerr << "CVector::Initialize(const double &): " << "invalid number of element" << nElm << endl;
+  if(vec_val == NULL){
+    if(size <= 0){
+      nElm = 0;
+      cerr << "CVector::Initialize(const unsigned long &, const double &): " << "invalid number of element" << nElm << endl;
+      throw(-1);
+    }
+    else{
+      nElm = size;
+      vec_val = new double[nElm];
+      for(unsigned int ii=0; ii<nElm; ii++){
+        vec_val[ii] = val;
+      }
+    }
+  }
+  else{
+    std::cerr << "CVector::Initialize(const unsigned long &, const double &): " << "vector already initialize, resizing is not allowed." << std::endl;
     throw(-1);
   }
-
-  for(unsigned int i= 0; i < nElm; i++)
-    vec_val[i] = val;
 }
 
 unsigned long CVector::GetSize() const{
+
   if(nElm <= 0){
     cerr << "CVector::GetSize() const: " << "invalid number of element" << nElm << endl;
     throw(-1);
@@ -81,35 +79,33 @@ double* CVector::GetVec() const{
 }
 
 void CVector::print() const{
+  cout << "**********" << endl;
   for(unsigned int i=0; i<nElm; i++)
     cout << vec_val[i] << endl;
+  cout << "**********" << endl;
+}
+
+void CVector::SetAllValues(const double & val){
+
+  for(unsigned int ii=0; ii<nElm; ii++){
+    vec_val[ii] = val;
+  }
 }
 
 CVector & CVector::operator=(const CVector & u){
   if(this == &u) return *this;
 
   if(nElm != u.nElm){
-    delete [] vec_val;
-    nElm = u.nElm;
-    vec_val = new double[nElm];
+     cerr << "Sizes do not match for operator = : size1 = " << nElm << " & size2 = " << u.nElm << endl;
+     throw(-1);
   }
-
-  for(unsigned int i=0; i<nElm; i++)
-    vec_val[i] = u.vec_val[i];
+  else{
+    for(unsigned int i=0; i<nElm; i++)
+      vec_val[i] = u.vec_val[i];
+  }
 
   return *this;
 }
-
-CVector & CVector::operator+(const CVector & u) const{
-   if(nElm != u.nElm){
-    cerr << "CVector::operator+(const CVector &) const: " << "sizes do not match" << endl;
-    throw(-1);
-   }
-
-   CVector sum(*this);
-   sum += u;
-   return sum;
- }
 
 CVector & CVector::operator+=(const CVector & u){
    if(nElm != u.nElm){
@@ -123,19 +119,7 @@ CVector & CVector::operator+=(const CVector & u){
     return *this;
  }
 
-CVector & CVector::operator-(const CVector & u) const{
-   if(nElm != u.nElm){
-    cerr << "CVector::operator-(const CVector &) const: " << "sizes do not match" << endl;
-    throw(-1);
-   }
-
-   CVector diff(*this);
-   diff -= u;
-
-   return diff;
- }
-
- CVector & CVector::operator-=(const CVector & u){
+CVector & CVector::operator-=(const CVector & u){
    if(nElm != u.nElm){
     cerr << "CVector::operator-=const CVector &) const: " << "sizes do not match" << endl;
     throw(-1);
@@ -147,29 +131,11 @@ CVector & CVector::operator-(const CVector & u) const{
    return *this;
  }
 
-CVector & CVector::operator*(const double & val) const{
-  CVector prod(*this);
-  prod *= val;
-  return prod;
-}
-
-/*CVector & CVector::operator*(const double & val, const CVector & u){
-  CVector prod(u);
-  prod *= val;
-  return prod;
-}*/
-
 CVector & CVector::operator*=(const double & val){
   for(unsigned int i=0; i<nElm; i++)
     vec_val[i] *= val;
 
   return *this;
-}
-
-CVector & CVector::operator/(const double & val) const{
-  CVector quotient(*this);
-  quotient /= val;
-  return quotient;
 }
 
 CVector & CVector::operator/=(const double & val){
@@ -183,22 +149,22 @@ double & CVector::operator[](const unsigned long & i) const{
   return vec_val[i];
 }
 
-double CVector::dotProd(const CVector & u, const CVector & v) const{
-  if(u.nElm != v.nElm){
+double CVector::dotProd(const CVector & v) const{
+  if(nElm != v.nElm){
     cerr << "CVector::dotProd(const CVector &, const CVector &): " << "sizes do not math" << endl;
     throw(-1);
   }
 
-  double dotProd(0);
-  for(unsigned int i=0; i<u.nElm; i++)
-    dotProd += u.vec_val[i]*v.vec_val[i];
+  double dotProd(0.0);
+  for(unsigned int i=0; i<nElm; i++)
+    dotProd += vec_val[i]*v.vec_val[i];
 
   return dotProd;
 }
 
 double CVector::norm() const{
   double norm(0), tempVal(0);
-  tempVal = dotProd(*this,*this);
+  tempVal = (*this).dotProd(*this);
 
   if(tempVal < 0){
     cerr << "CVector::norm(): " << "computed dot product is negative: " << tempVal << endl;
@@ -217,68 +183,95 @@ void CVector::Reset(){
 
 /*--- Definition of the CMatrix class ---*/
 
-CMatrix::CMatrix(void){
-  mat_val = NULL;
-}
+CMatrix::CMatrix(void):nEq(0), nVar(0){}
 
 CMatrix::CMatrix(const unsigned long & val_nEq, const unsigned long & val_nVar, const double & val){
   nEq = val_nEq;
   nVar = val_nVar;
   unsigned long nElm = nEq*nVar;
 
-  mat_val = new CVector(nElm,val);
+  mat_val.Initialize(nElm, val);
 }
 
-CMatrix::CMatrix(const unsigned long & val_nEq, const unsigned long & val_nVar, const double* a_array){
+CMatrix::CMatrix(const CMatrix & A){
+
+  nEq = A.nEq;
+  nVar = A.nVar;
+  unsigned long nElm = nEq*nVar;
+
+  mat_val.Initialize(nElm, 0.0);
+  mat_val = A.mat_val;
+}
+
+CMatrix::~CMatrix(){}
+
+void CMatrix::Initialize(const unsigned long & val_nEq, const unsigned long & val_nVar, const double & val){
+
   nEq = val_nEq;
   nVar = val_nVar;
-  const unsigned long nElm = nEq*nVar;
-  mat_val = new CVector(nElm, a_array);
-}
+  unsigned long nElm = nEq*nVar;
 
-CMatrix::~CMatrix(){
-  delete mat_val;
-  mat_val = NULL;
+  mat_val.Initialize(nElm, val);
 }
 
 void CMatrix::print() const{
-  mat_val->print();
+  mat_val.print();
 }
 
 CMatrix & CMatrix::operator=(const CMatrix & a){
   if(this == &a) return *this;
 
   if(nEq != a.nEq || nVar != a.nVar){
-    delete mat_val;
-    nEq = a.nEq;
-    nVar = a.nVar;
-    mat_val = new CVector(nEq*nVar);
+    cerr << "Sizes do not match for operator = : nEq1 = " << nEq << " & nEq2 = " << a.nEq << " ; nVar1 = " << nVar << " & nVar2 = " << a.nVar << endl;
+    throw(-1);
   }
-
-  *mat_val = *(a.mat_val);
-
+  else{
+    mat_val = a.mat_val;
+  }
 
   return *this;
 }
 
-CMatrix & CMatrix::operator+(const CMatrix & a) const{
+CMatrix & CMatrix::operator+=(const CMatrix & a){
+
   if(nEq != a.nEq || nVar != a.nVar){
-    cerr << "CMatrix::operator+(const CMatrix &) const: " << "sizes do not match" << endl;
+    cerr << "CMatrix::operator+=(const CMatrix &): " << "sizes do not match" << endl;
     throw(-1);
   }
+  else{
+    mat_val += a.mat_val;
+  }
 
-  CMatrix sum(*this);
-  sum += a;
-  return sum;
+  return *this;
 }
 
-CMatrix & CMatrix::operator+=(const CMatrix & a){
+CMatrix & CMatrix::operator-=(const CMatrix & a){
+
   if(nEq != a.nEq || nVar != a.nVar){
-    cerr << "CMatrix::operator+(const CMatrix &) const: " << "sizes do not match" << endl;
+    cerr << "CMatrix::operator+=(const CMatrix &): " << "sizes do not match" << endl;
     throw(-1);
   }
+  else{
+    mat_val -= a.mat_val;
+  }
 
-  *mat_val += *(a.mat_val);
+  return *this;
+}
+
+CMatrix & CMatrix::operator*=(const double & val){
+
+  mat_val *= val;
+
+  return *this;
+
+}
+
+CMatrix & CMatrix::operator/=(const double & val){
+
+  mat_val /= val;
+
+  return *this;
+
 }
 
 unsigned long CMatrix::GetnEq() const{
@@ -290,25 +283,25 @@ unsigned long CMatrix::GetnVar() const{
 }
 
 double* CMatrix::GetMat() const{
-  return mat_val->GetVec();
+  return mat_val.GetVec();
 }
 
-CVector* CMatrix::GetCVec() const{
+CVector CMatrix::GetCVec() const{
   return mat_val;
 }
 
-void CMatrix::SetElm(int i, int j, double val){
-  (*mat_val)[(j-1)*nEq+(i-1)] = val;
+void CMatrix::SetElm(const int &i, const int &j, double val){
+  mat_val[(j-1)*nEq+(i-1)] = val;
 }
 
-double CMatrix::GetElm(int i, int j) const{
-  return (*mat_val)[(j-1)*nEq+(i-1)];
+double CMatrix::GetElm(const int &i, const int &j) const{
+  return mat_val[(j-1)*nEq+(i-1)];
 }
 
 double CMatrix::DiagProduct() const{
   double prod(1);
   if (nEq == nVar){
-    for(int i=1; i<=nEq; i++){
+    for(unsigned int i=1; i<=nEq; i++){
       prod *= GetElm(i,i);
     }
   }
@@ -323,55 +316,58 @@ double CMatrix::ComputeDet() const{
   int IPIV[nEq];
   int INFO(-1);
 
-  *PLU = *(mat_val->GetVec());
+  *PLU = *(mat_val.GetVec());
   INFO = LAPACKE_dgetrf(LAPACK_COL_MAJOR,nEq,nVar,PLU,nEq,IPIV);
 
   for(int i=0; i<nEq-1; i++){
     if(IPIV[i] != i+1) NbChange++;
   }
+
+  return 0.0;
 }
 
 void CMatrix::Reset(){
-  mat_val->Reset();
+  mat_val.Reset();
 }
 
-CVector MatVecProd(CMatrix* A, CVector* b){
-  CVector x(A->GetnVar());
-  cblas_dgemv(CblasColMajor,CblasNoTrans,A->GetnEq(),A->GetnVar(),1,A->GetMat(),A->GetnEq(),b->GetVec(),1,0,x.GetVec(),1);
+CVector MatVecProd(const CMatrix &A, const CVector &b){
 
-   return x;
-}
-
-CVector ScalVecProd(const double & scal, CVector* b){
-  CVector x(b->GetSize());
-  cblas_daxpy(b->GetSize(),scal,b->GetVec(),1,x.GetVec(),1);
+  CVector x(A.GetnVar());
+  cblas_dgemv(CblasColMajor,CblasNoTrans,A.GetnEq(),A.GetnVar(),1,A.GetMat(),A.GetnEq(),b.GetVec(),1,0,x.GetVec(),1);
   return x;
+
 }
 
-CMatrix ScalMatProd(const double & scal, CMatrix* A){
-  CMatrix U(A->GetnEq(),A->GetnVar(), A->GetMat());
-  CVector* temp = U.GetCVec();
-  *temp *= scal;
+CVector ScalVecProd(const double & scal, const CVector &b){
 
-  return U;
+  CVector x(b.GetSize());
+  cblas_daxpy(b.GetSize(),scal,b.GetVec(),1,x.GetVec(),1);
+  return x;
+
 }
 
-int SolveSys(CMatrix *A, CVector *b){
-  const int N = A->GetnEq();
-  const int LDA = A->GetnVar();
-  CMatrix* Mat;
-  Mat = new CMatrix(N,LDA,A->GetMat());
+CMatrix ScalMatProd(const double & scal, const CMatrix &A){
+
+  CMatrix copy(A);
+  copy *= scal;
+  return copy;
+}
+
+/* INDEPENDENT FUNCTIONS */
+int SolveSys(const CMatrix &A, CVector &b){
+  const int N = A.GetnEq();
+  const int LDA = A.GetnVar();
+  CMatrix Mat(A);
   int NRHS(1);
   int *IPIV;
-  int LDB = b->GetSize();
+  int LDB = b.GetSize();
   int INFO(-1);
 
   IPIV = new int[N];
 
-  INFO = LAPACKE_dgesv(LAPACK_COL_MAJOR,N,NRHS,Mat->GetMat(),LDA,IPIV,b->GetVec(),LDB);
+  INFO = LAPACKE_dgesv(LAPACK_COL_MAJOR,N,NRHS,Mat.GetMat(),LDA,IPIV,b.GetVec(),LDB);
 
-  delete Mat;
-  Mat = NULL;
+  return INFO;
 }
 
 void MatrixToVec(int order, double** matrix, double* vecteur, int Nrow, int Ncol, int sizeVec){
@@ -404,4 +400,180 @@ void VecToMatrix(int order, double** matrix, double* vecteur, int Nrow, int Ncol
       cerr << "Wrong storage order" << endl;
       throw(-1);
     }
+}
+
+/* OPERATORS OVERLOADS*/
+CVector operator+(CVector & vecA, CVector & vecB){
+
+  CVector copy(vecA);
+  copy += vecB;
+  return copy;
+}
+
+CVector operator+(const CVector & vecA, const CVector & vecB){
+
+  CVector copy(vecA);
+  copy += vecB;
+  return copy;
+}
+
+CVector operator+(CVector & vecA, const CVector & vecB){
+
+  CVector copy(vecA);
+  copy += vecB;
+  return copy;
+}
+
+CVector operator+(const CVector & vecA, CVector & vecB){
+
+  CVector copy(vecA);
+  copy += vecB;
+  return copy;
+}
+
+
+
+CVector operator-(CVector & vecA, CVector & vecB){
+
+  CVector copy(vecA);
+  copy -= vecB;
+  return copy;
+}
+
+CVector operator-(const CVector & vecA, CVector & vecB){
+
+  CVector copy(vecA);
+  copy -= vecB;
+  return copy;
+}
+
+CVector operator-(CVector & vecA, const CVector & vecB){
+
+  CVector copy(vecA);
+  copy -= vecB;
+  return copy;
+}
+
+CVector operator-(const CVector & vecA, const CVector & vecB){
+
+  CVector copy(vecA);
+  copy -= vecB;
+  return copy;
+}
+
+
+
+CVector operator*(CVector & vecA, double & val_mult){
+
+  CVector copy(vecA);
+  copy *= val_mult;
+  return copy;
+}
+
+CVector operator*(CVector & vecA, const double & val_mult){
+
+  CVector copy(vecA);
+  copy *= val_mult;
+  return copy;
+}
+
+CVector operator*(double & val_mult, CVector & vecA){
+
+  CVector copy(vecA);
+  copy *= val_mult;
+  return copy;
+}
+
+CVector operator*(const double & val_mult, CVector & vecA){
+
+  CVector copy(vecA);
+  copy *= val_mult;
+  return copy;
+}
+
+CVector operator*(const CVector & vecA, double & val_mult){
+
+  CVector copy(vecA);
+  copy *= val_mult;
+  return copy;
+}
+
+CVector operator*(const CVector & vecA, const double & val_mult){
+
+  CVector copy(vecA);
+  copy *= val_mult;
+  return copy;
+}
+
+CVector operator*(double & val_mult, const CVector & vecA){
+
+  CVector copy(vecA);
+  copy *= val_mult;
+  return copy;
+}
+
+CVector operator*(const double & val_mult, const CVector & vecA){
+
+  CVector copy(vecA);
+  copy *= val_mult;
+  return copy;
+}
+
+
+
+CVector operator/(CVector & vecA, double & val_mult){
+
+  CVector copy(vecA);
+  copy /= val_mult;
+  return copy;
+}
+
+CVector operator/(CVector & vecA, const double & val_mult){
+
+  CVector copy(vecA);
+  copy /= val_mult;
+  return copy;
+}
+
+CVector operator/(const CVector & vecA, double & val_mult){
+
+  CVector copy(vecA);
+  copy /= val_mult;
+  return copy;
+}
+
+CVector operator/(const CVector & vecA, const double & val_mult){
+
+  CVector copy(vecA);
+  copy /= val_mult;
+  return copy;
+}
+
+
+CMatrix operator+(CMatrix & matA, CMatrix & matB){
+
+  CMatrix copy(matA);
+  copy += matB;
+  return copy;
+}
+
+CMatrix operator-(CMatrix & matA, CMatrix & matB){
+
+  CMatrix copy(matA);
+  copy -= matB;
+  return copy;
+}
+
+CMatrix operator*(CMatrix matA, double & val){
+
+  CMatrix copy(matA);
+  copy *= val;
+  return copy;
+}
+
+CMatrix operator*(double & val, CMatrix matA){
+
+  CMatrix copy(matA);
+  copy *= val;
+  return copy;
 }
